59c1874e2e15354ad7a748e6cdc55f374e3f3c3a
ribru17 <ribru17@hotmail.com>
2023-02-19 15:50:45 -0800

    initial landing page design

--- src/components/pages/landing/index.js
+++ src/components/pages/landing/index.js
@@ -1,9 +1,18 @@
 import React from "react";
+import "./index.css"
 
 export default function Main() {
     return (
-        <div>
-            <h1>Hello!</h1> 
-        </div>
+        <main>
+            <nav id="navBar">
+                <a href="/" style={{marginLeft: 50, fontSize: 59}}>DentalSite</a>
+                <div style={{display: 'flex', justifyContent: 'space-evenly', width: 500, fontSize: 30}}>
+                    <a href="about">About</a>
+                    <a href="contact">Contact</a>
+                    <a href="register">Sign Up</a>
+                </div>
+            </nav>
+            <h1>Booking Reimagined.</h1> 
+        </main>
     )
 }
