bd9b7a70e450831ed4b5de921375e7e165ddf5a8
Bryan <leeteng2001@gmail.com>
2023-04-05 18:41:59 +0800

    redirect signin for login user, dynamic scheudle data, force login to refresh context

--- src/global/AuthContext.js
+++ src/global/AuthContext.js
@@ -5,28 +5,29 @@ export const AuthContext = React.createContext([]);
 export const AuthContextProvider = (props) => {
   const [ currentUser, setCurrentUser ] = useState({loading: true, data: null});
 
-  useEffect(() => {
-    async function fetchUserInfo() {
-      // TODO: Save state inside localstorage to avoid constant info fetching?
-      let userInfo = await fetch(`${process.env.REACT_APP_BASE_URL}/api/auth/info`, {
-        method: 'GET',
-        credentials: 'include',
-      }).then(dt => dt.json());
-      console.log("AuthContextProvider userinfo", userInfo)
-      if (userInfo.success) {
-        console.log("Found login user", userInfo.data)
-        setCurrentUser(state => ({ ...state, loading: false, data: userInfo.data }));
-      }
-      else {
-        console.log("No login user")
-        setCurrentUser(state => ({ ...state, loading: false, data: null }));
-      }
+  async function fetchUserInfo() {
+    // TODO: Save state inside localstorage to avoid constant info fetching?
+    let userInfo = await fetch(`${process.env.REACT_APP_BASE_URL}/api/auth/info`, {
+      method: 'GET',
+      credentials: 'include',
+    }).then(dt => dt.json());
+    console.log("AuthContextProvider userinfo", userInfo)
+    if (userInfo.success) {
+      console.log("Found login user", userInfo.data)
+      setCurrentUser(state => ({ ...state, loading: false, data: userInfo.data }));
     }
+    else {
+      console.log("No login user")
+      setCurrentUser(state => ({ ...state, loading: false, data: null }));
+    }
+  }
+
+  useEffect(() => {
     fetchUserInfo();
   }, []);
 
   return (
-    <AuthContext.Provider value={[currentUser, setCurrentUser]}>
+    <AuthContext.Provider value={[currentUser, setCurrentUser, fetchUserInfo]}>
       {props.children}
     </AuthContext.Provider>
   )
