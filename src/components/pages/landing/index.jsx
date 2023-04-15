94360effdab6f2d157c6b5ec7453495fef3b5a00
Anan Wang <mengan.wang@gmail.com>
2023-04-15 16:50:51 -0700

    join waitlist

--- src/components/pages/landing/index.jsx
+++ src/components/pages/landing/index.jsx
@@ -61,7 +61,7 @@ export default function Main() {
                             }}
                             id="bookNow"
                         >
-                            Book Now
+                            Join The Waitlist
                         </button>
                         {/* <button id="learnMore">Learn More</button> */}
                     </div>
